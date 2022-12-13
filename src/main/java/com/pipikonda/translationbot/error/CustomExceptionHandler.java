9802b936fef6d174945f346926483f3769d76974
Nikolay Naumenko <mykola.naumenko@monobank.ua>
2022-12-13 21:56:46 +0200

    add exception telegram logging https://github.com/pipikonda/translation-service/issues/16

--- src/main/java/com/pipikonda/translationbot/error/CustomExceptionHandler.java
+++ src/main/java/com/pipikonda/translationbot/error/CustomExceptionHandler.java
@@ -1,17 +1,35 @@
 package com.pipikonda.translationbot.error;
 
+import com.pipikonda.translationbot.telegram.TranslateBot;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @Slf4j
 public class CustomExceptionHandler {
+
+    private final TranslateBot translateBot;
+    private final MessageSource messageSource;
+    private final String chatId;
+
+    public CustomExceptionHandler(TranslateBot translateBot,
+                                  MessageSource messageSource,
+                                  @Value("${service.telegram.exceptions.chat-id}") String chatId) {
+        this.translateBot = translateBot;
+        this.messageSource = messageSource;
+        this.chatId = chatId;
+    }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BasicLogicException.class)
@@ -37,9 +55,15 @@ public class CustomExceptionHandler {
                 .build();
     }
 
-    public ErrorResponse handle(Exception exception) {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ErrorResponse handle(Exception exception) throws TelegramApiException {
         logException(exception);
-        //send message
+        Object[] params = new Object[]{exception.getCause().getClass(), exception.getMessage()};
+        translateBot.execute(SendMessage.builder()
+                .chatId(chatId)
+                .text(messageSource.getMessage("telegram.exception.message-text", params, Locale.getDefault()))
+                .build());
         return ErrorResponse.builder()
                 .errorCode(ErrorCode.UNKNOWN_ERROR)
                 .errorText(exception.getMessage())
