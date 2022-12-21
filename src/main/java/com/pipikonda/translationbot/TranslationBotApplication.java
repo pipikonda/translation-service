859f562d2f148e1765f144cc1696ab7684905989
Nikolay Naumenko <mykola.naumenko@monobank.ua>
2022-12-21 22:31:45 +0200

    fix timezone

--- src/main/java/com/pipikonda/translationbot/TranslationBotApplication.java
+++ src/main/java/com/pipikonda/translationbot/TranslationBotApplication.java
@@ -3,10 +3,13 @@ package com.pipikonda.translationbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class TranslationBotApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+2:00"));
         SpringApplication.run(TranslationBotApplication.class, args);
     }
 
